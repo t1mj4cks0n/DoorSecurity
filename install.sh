af41a9d72f5566b471bba9fa852f13f6aa6285d4
Tim Jackson <34356285+t1mj4cks0n@users.noreply.github.com>
2018-11-02 19:50:15 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -21,6 +21,10 @@ mkdir /home/pi/DoorSecurity/logs/
 cd /home/pi/DoorSecurity/logs/
 touch debugs_logs door_logs scp_logs
 mkdir vids archives
+cd /home/pi/DoorSecurity/doormodules/
+touch __init__.py
+cd /home/pi/DoorSecurity/test/
+touch __init__.py
 
 # script permission
 cd /home/pi/DoorSecurity/
