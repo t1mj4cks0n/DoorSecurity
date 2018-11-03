b99a29254eebcb0936087e791e362d5a5fb33d95
Tim Jackson <34356285+t1mj4cks0n@users.noreply.github.com>
2018-11-03 02:22:46 +0000

    changing permissions
    installer now needs to be ran with sudo
    some of the commands run as user

--- install.sh
+++ install.sh
@@ -1,48 +1,58 @@
 #!/bin/sh
-
-# This needs to be ran immediately after repo download
-echo "installing python dependencies"
-sleep 2
-# install python and camera dependencies
-sudo apt install python-dev python-rpi.gpio -y
-sudo apt install ffmpeg python-pip -y
-sudo pip install pip-update
-sudo pip install setuptools wheels
-
-echo "installing paramiko for scp connections"
-sleep 2
-# install paramiko and dependencies
-cd /home/$USER/
-sudo git clone https://github.com/paramiko/paramiko
-echo "installing paramiko dependencies and setting it up now"
-sudo apt install build-essential libssl-dev libffi-dev -y
-cd /home/$USER/paramiko && sudo python setup.py install
-
-# copy config files | set permissions
-echo "Copying files to system location for autorunning"
-sleep 2
-sudo cp /home/$USER/DoorSecurity/confs/doorsensor.service /etc/systemd/system/doorsensor.service
-sudo chmod +x /etc/systemd/system/doorsensor.service
-sudo cp /home/$USER/DoorSecurity/confs/start_door_sensor.bash /root/start_door_sensor.bash
-sudo chmod +x /root/start_door_sensor.bash
-
-echo "setting permission of scripts"
-# script permission
-cd /home/$USER/DoorSecurity/
-sudo chmod +x recordcamera.sh
-
-#start services
-echo "reloading system services"
-sudo system daemon-reload
-sudo systemctl enable doorsensor.service
-sudo systemctl start doorsensor.service
-echo "doorsensor.service STATUS below, Q to carry on..."
-sudo systemctl status doorsensor.service
-echo "opening nano on this file ' /home/$USER/DoorSecurity/doormodules/config.py'\n fill this out to configure the program\n PRESS ENTER TO CONTINUE"
-read yes
-sudo nano /home/$USER/DoorSecurity/doormodules/config.py
-
-sudo rm install.sh
+echo "##########################################################"
+echo " 	installer for DoorSecurity"
+echo " 	this should be run as root with the sudo cmd"
+echo "##########################################################"
+echo \nYou are :$USER
+echo This should say root, what is your username? i.e pi
+read homeuser
+username=$homeuser
+echo You are $username@$hostname
+echo "##########################################################"
+echo 			install python and camera dependencies
+echo "##########################################################"
+apt install python-dev python-rpi.gpio -y
+apt install ffmpeg python-pip -y
+pip install pip-update
+pip install setuptools
+cd /home/$username/
+sleep 5
+echo "##########################################################"
+echo 			install paramiko and dependencies
+echo "##########################################################"
+su - $username -c "git clone https://github.com/paramiko/paramiko"
+echo Installing paramiko dependencies and setting it up now
+apt install build-essential libssl-dev libffi-dev -y
+cd /home/$username/paramiko && sudo python setup.py install
+sleep 5
+echo "##########################################################"
+echo 				Configuring File system
+echo "##########################################################"
+cp /home/$username/DoorSecurity/confs/doorsensor.service /etc/systemd/system/doorsensor.service
+chmod +x /etc/systemd/system/doorsensor.service
+cat <<EOF >/root/start_door_sensor.bash
+#!/bin/bash
+cd /home/$username/DoorSecurity
+sudo python door_sensor.py
+EOF
+su - $username -c "mkdir /home/$username/DoorSecurity/logs"
+cd /home/$username/DoorSecurity/logs
+su - $username -c "mkdir archives/ vids/"
+cd /home/$username/DoorSecurity/
+su - $username -c "chmod +x recordcamera.sh"
+sleep 5
+echo "##########################################################"
+echo 				Restarting the Services
+echo "##########################################################"
+systemctl daemon-reload
+systemctl enable doorsensor.service
+systemctl start doorsensor.service
+echo doorsensor.service STATUS below, Q to carry on...
+systemctl status doorsensor.service
+echo opening nano on this file  /home/$username/DoorSecurity/doormodules/config.py\n fill this out to configure the program\n PRESS ENTER TO CONTINUE
+sleep 5
+su - $username -c "nano /home/$username/DoorSecurity/doormodules/config.py"
+rm install.sh
 
 
 
